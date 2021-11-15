e6b7c05e9001626161f5d9efc3e720ad29e6cec3
cosmos <18826234801@163.com>
2021-11-15 20:20:55 +0800

    04-数据结构_两个栈实现队列

---
+++ src/data_structure/TwoStackImplementQueue.java
@@ -0,0 +1,65 @@
+package data_structure;
+
+import java.util.Stack;
+
+/**
+ * 两个栈实现队列
+ */
+
+public class TwoStackImplementQueue {
+    
+    public static class TwoStacksQueue {
+
+        public Stack<Integer> stackPush;
+        public Stack<Integer> stackPop;
+
+        public TwoStacksQueue() {
+            stackPush = new Stack<>();
+            stackPop = new Stack<>();
+        }
+
+        public void pushToPop() {
+            // stackPop 为空时，将 stackPush 中的数据全部导入stackPop
+            if (stackPop.empty()) {
+                while (!stackPush.empty()) {
+                    stackPop.push(stackPush.pop());
+                }
+            }
+        }
+
+        public void add(int pushInt) {
+            stackPush.push(pushInt);
+            pushToPop();
+        }
+
+        public int poll() {
+            if (stackPop.empty() && stackPush.empty()) {
+                throw new RuntimeException("queue is empty!");
+            }
+            pushToPop();
+            return stackPop.pop();
+        }
+
+        public int peek() {
+            if (stackPop.empty() && stackPush.empty()) {
+                throw new RuntimeException("queue is empty!");
+            }
+            pushToPop();
+            return stackPop.peek();
+        }
+
+    }
+
+    public static void main(String[] args) {
+        TwoStacksQueue test = new TwoStacksQueue();
+		test.add(1);
+		test.add(2);
+		test.add(3);
+		System.out.println(test.peek());
+		System.out.println(test.poll());
+		System.out.println(test.peek());
+		System.out.println(test.poll());
+		System.out.println(test.peek());
+		System.out.println(test.poll());
+    }
+}
