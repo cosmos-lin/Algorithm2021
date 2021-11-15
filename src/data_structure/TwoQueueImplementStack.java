1ab0dc27034a6dc4d05623619ffad5e00f39bffb
cosmos <18826234801@163.com>
2021-11-15 20:21:27 +0800

    04-数据结构_两个队列实现栈

---
+++ src/data_structure/TwoQueueImplementStack.java
@@ -0,0 +1,94 @@
+package data_structure;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+/**
+ * 两个队列实现栈
+ */
+public class TwoQueueImplementStack {
+    
+    public static class TwoQueueStack<T> {
+        public Queue<T> queue;
+        public Queue<T> help;
+
+        public TwoQueueStack() {
+            queue = new LinkedList<>();
+            help = new LinkedList<>();
+        }
+
+        public void push(T value) {
+            queue.offer(value);
+        }
+
+        public T poll() {
+            // 当queue 元素个数n > 1 将队列中的 n-1个元素放入到help, 剩下一个弹出
+            while (queue.size() > 1) {
+                help.offer(queue.poll());
+            }
+            T ans = queue.poll();
+            // 交换两个队列的引用
+            Queue<T> tmp = queue;
+            queue = help;
+            help = tmp;
+            return ans;
+        }
+
+        public T peek() {
+            while (queue.size() > 1) {
+                help.offer(queue.poll());
+            }
+            T ans = queue.poll();
+            help.offer(ans);
+            // 交换两个队列的引用
+            Queue<T> tmp = queue;
+            queue = help;
+            help = tmp;
+            return ans;
+        }
+
+        public boolean isEmpty() {
+            return queue.isEmpty();
+        }
+    }
+
+    public static void main(String[] args) {
+		System.out.println("test begin");
+		TwoQueueStack<Integer> myStack = new TwoQueueStack<>();
+		Stack<Integer> test = new Stack<>();
+		int testTime = 1000000;
+		int max = 1000000;
+		for (int i = 0; i < testTime; i++) {
+			if (myStack.isEmpty()) {
+				if (!test.isEmpty()) {
+					System.out.println("Oops");
+				}
+				int num = (int) (Math.random() * max);
+				myStack.push(num);
+				test.push(num);
+			} else {
+				if (Math.random() < 0.25) {
+					int num = (int) (Math.random() * max);
+					myStack.push(num);
+					test.push(num);
+				} else if (Math.random() < 0.5) {
+					if (!myStack.peek().equals(test.peek())) {
+						System.out.println("Oops");
+					}
+				} else if (Math.random() < 0.75) {
+					if (!myStack.poll().equals(test.pop())) {
+						System.out.println("Oops");
+					}
+				} else {
+					if (myStack.isEmpty() != test.isEmpty()) {
+						System.out.println("Oops");
+					}
+				}
+			}
+		}
+
+		System.out.println("test finish!");
+
+	}
+}
